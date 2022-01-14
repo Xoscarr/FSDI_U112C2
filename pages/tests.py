4503580eb96bffee73f97b4366eb08bb911e3279
Oscar Rodriguez <rrodriguezsame@gmail.com>
2022-01-13 19:42:12 -0800

    add posts tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse 
 
-# Create your tests here.
+class PagesTests(SimpleTestCase):
+
+    def test_home_page_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template_used(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+    
+    def test_home_page_uses_base(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_home_page_content(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Write your messages here")
+    
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        response_from_path = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(response, "home.html")
+
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from .models import Post 
+from django.urls import reverse
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title="A test", body="Test")
+    
+    def test_body_content(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f"{post.body}"
+        self.assertEqual(expected_object_name, "Test")
+    
+    def test_post_string_representation(self):
+        post = Post.objects.get(id=1)
+        str_repr = str(post)
+        self.assertEqual(str_repr, "A test")
+
+class PostViewTest(TestCase): 
+    def setUp(self):
+        Post.objects.create(title="Another test", body="Test2")
+
+    def test_listview_url_exists_at_proper_location(self):
+        resp = self.client.get("/posts/")
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_listview_url_by_name(self):
+        resp = self.client.get(reverse("posts_list"))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_listview_uses_correct_template(self):
+        resp = self.client.get("/posts/")
+        self.assertTemplateUsed(resp, "post_list.html")
+
+    def test_listview_contains_content(self):
+        resp = self.client.get(reverse("posts_list"))
+        self.assertContains(resp, "Another test")
+    
+    def test_detailview_exists_at_proper_location(self):
+        resp = self.client.get("/posts/1/")
+        self.assertEqual(resp.status_code, 200)
+
+    def test_detailview_uses_correct_template(self):
+        resp = self.client.get(reverse("post_detail", args=[1]))
+        self.assertTemplateUsed(resp, "post_detail.html")
+
+    def test_detailview_contains_content(self):
+        resp = self.client.get(reverse("post_detail", args=[1]))
+        self.assertContains(resp, "Another test")
